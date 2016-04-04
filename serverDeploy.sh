c7e21ed1a6be79291cb9e79df3063b2088c4f62c
Kris Payne <krispayne@me.com>
2016-04-04 16:04:31 -0700

    setting abstract for script

--- serverDeploy.sh
+++ serverDeploy.sh
@@ -3,7 +3,13 @@
 
 # Variables
 
-#
+# Settings we are going to need to edit
+# Casper Distribution Point (http)
+# Caching service
+# Viritual Box
+# Windows VM
+# Zello VM
+# Presto 2
 
 # Script - Do not edit below this line
 
