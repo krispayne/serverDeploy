e0108d27f97633d486cabd831cbd79a75c80c8d3
Kris Payne <krispayne@me.com>
2016-04-04 16:42:46 -0700

    cleaning

--- serverDeploy.sh
+++ serverDeploy.sh
@@ -14,21 +14,39 @@
 # Presto 2 - 0%
 
 # Bring in our other dependencies
-#source expected1.sh
+
 
 # Start setting up Server.app (Caching)
 
 serverCachingSetup() {
-    # start the service
-    serveradmin start caching
 
-    # define some settings
-    serveradmin settings caching:ServerRoot = "/Library/Server"
-    serveradmin settings caching:LocalSubnetsOnly = yes
-    serveradmin settings caching:AllowPersonalCaching = no
-    serveradmin settings caching:DataPath = "/Library/Server/Caching/Data"
-    serveradmin settings caching:ReservedVolumeSpace = 25000000000
-    serveradmin settings caching:CacheLimit = 53687091200
+    # Setup Server.app
+    #source expected1.sh
+
+    # start the service
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin start caching
+
+    # Set location of the ServerRoot
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings caching:ServerRoot = "/Library/Server"
+
+    # Set location of the Cache data
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings caching:DataPath = "/Library/Server/Caching/Data"
+
+    # Set to only supply and recieve cache from local subnets
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings caching:LocalSubnetsOnly = yes
+
+    # Disable personal caching
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings caching:AllowPersonalCaching = no
+
+    # Reservered Volume Space - Needs research
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings caching:ReservedVolumeSpace = 10000000000
+
+    # Cache limit of ~50GB
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings caching:CacheLimit = 50000000000
+
+    # restart the service
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin stop caching
+    /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin start caching
 
 }
 
