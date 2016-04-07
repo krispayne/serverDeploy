41a7a2e1380e4650ae78ba1877db89027c3367d7
Kris Payne <krispayne@me.com>
2016-04-07 11:45:06 -0700

    fix quotes around username/pass

--- expected1.sh
+++ expected1.sh
@@ -4,6 +4,6 @@ set timeout 300
 spawn /Applications/Server.app/Contents/ServerRoot/usr/sbin/server setup
 expect "Press Return to view the software license agreement." { send \r }
 expect "Do you agree to the terms of the software license agreement? (y/N)" { send "y\r" }
-expect "User name:" { send "MYADMINUSERNAME\r" }
-expect "Password:" { send "MYPASSWORD\r" }
+expect "User name:" { send MYADMINUSERNAME\r }
+expect "Password:" { send MYPASSWORD\r }
 interact
